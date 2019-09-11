0d3d5100bd08d1cd5043a1e09a9d20b82181d274
SandipCodes <sandipsh321@gmail.com>
2019-09-11 15:20:35 +0530

    SENDING MAIL WITH ATTACHEMENT

--- Task5-JavaMailSenderDemo2-WEBAPP/src/main/java/com/app/service/impl/UserServiceImpl.java
+++ Task5-JavaMailSenderDemo2-WEBAPP/src/main/java/com/app/service/impl/UserServiceImpl.java
@@ -1,8 +1,13 @@
 package com.app.service.impl;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
 import com.app.model.User;
@@ -16,8 +21,14 @@ public class UserServiceImpl implements UserService {
 	
 	@Override
 	public String saveUser(User u) {
-		
-		String message=sendEmail(u);
+		String message=null;
+		try {
+			//String message=sendEmail(u);
+			 message=sendEmailWithAttachment(u);
+		}
+		catch(MessagingException me) {
+			me.printStackTrace();
+		}
 		return message;
 	}
 	
@@ -34,5 +45,24 @@ public class UserServiceImpl implements UserService {
         return "User saved and credential Message sent successfully to registered email id..";
 		
 	}//sendEmail()
+	
+	public String sendEmailWithAttachment(User u) throws MessagingException {
+		
+		MimeMessage message=javaMailSender.createMimeMessage();
+		
+		MimeMessageHelper helper=new MimeMessageHelper(message, true);
+		String[] to= {"sandiphumbe878@gmail.com","sandipsh321@yahoo.com"};
+		helper.setTo(to);
+		helper.setFrom("sandipsh321@gmail.com");
+		helper.setSubject("JAVA MAIL SENDER WEB APP DEMO WITH ATTACHEMENT");
+		helper.setText("<h1>Hi Sandip, This is a demo mail to check JAVA MAIL SENDER WEB APP FUNCTIONALITY.</h1>", true);
+	
+		helper.addAttachment("dreams.jpg", new ClassPathResource("static/dreams.jpg"));
+		
+		javaMailSender.send(message);
+
+	    return " Message sent successfully to registered email id..";
+		
+	}//sendEmailWithAttachement()
 
 }//class
